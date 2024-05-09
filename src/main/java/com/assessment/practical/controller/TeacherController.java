d47c2126e1f98f8dc7468bd307b25736395820b1
Chirath Fonseka <chirathfonseka98#gmail.com>
2024-05-09 15:43:13 +0530

    add log messages

--- src/main/java/com/assessment/practical/controller/TeacherController.java
+++ src/main/java/com/assessment/practical/controller/TeacherController.java
@@ -2,6 +2,10 @@ package com.assessment.practical.controller;
 
 import com.assessment.practical.entity.Teacher;
 import com.assessment.practical.service.TeacherService;
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,16 +15,20 @@ import java.util.List;
 @RestController
 @RequestMapping("/teacher")
 public class TeacherController {
+
+    public Logger logger = LoggerFactory.getLogger(TeacherController.class);
     @Autowired
     private TeacherService teacherService;
 
     @GetMapping ("/list")
     public List<Teacher> teacherList(){
+        logger.info("View teacher list API request received");
         return teacherService.teacherList ();
     }
 
     @PostMapping("/save")
     public Teacher addTeacher (@RequestBody Teacher teacher){
+        logger.info("Save teacher API request received");
         return teacherService.saveTeacher (teacher);
     }
 }
--- src/main/java/com/assessment/practical/service/TeacherService.java
+++ src/main/java/com/assessment/practical/service/TeacherService.java
@@ -1,7 +1,10 @@
 package com.assessment.practical.service;
 
+import com.assessment.practical.controller.TeacherController;
 import com.assessment.practical.entity.Teacher;
 import com.assessment.practical.repository.TeacherRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,14 +13,17 @@ import java.util.List;
 
 @Service
 public class TeacherService {
+    public Logger logger = LoggerFactory.getLogger(TeacherService.class);
     @Autowired
     private TeacherRepository teacherRepository;
 
     public List<Teacher> teacherList(){
+        logger.info ("Retrieve all teacher names from DB");
         return teacherRepository.findAll ();
     }
 
     public Teacher saveTeacher(@RequestBody Teacher teacher){
+        logger.info ("Save teacher name in DB");
         return teacherRepository.save (teacher);
     }
 }
